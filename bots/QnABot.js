9bb7deb13d1ebc2910ab2fefa38688407186ad02
Leon Jäkel <leon.jaekel@sparqs.io>
2020-03-23 16:00:45 +0100

    - Fix typo issues at welcome message from chatbot

--- bots/QnABot.js
+++ bots/QnABot.js
@@ -37,7 +37,10 @@ class QnABot extends ActivityHandler {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; cnt++) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity('Hallo! Ich bin Covi, dein ChatBot für Fragen rund um den Corona Virus 🙂 Ich bin noch ganz jung und weiß daher nicht alles. Aber ich gebe mir echt Mühe und lerne jeden Tag dazu! 💪! \n\n Hast du z. B. die Befürchtung dich infiziert zu haben, kann ich dir helfen zu beurteilen, ob du wirklich zur Risikogruppe gehört!');
+                    let welcomeText = 'Hallo! Ich bin Covi, dein ChatBot für Fragen rund um das Corona Virus 🙂';
+                    welcome += 'Ich bin noch ganz jung und weiß daher nicht alles. Aber ich gebe mir echt Mühe und lerne jeden Tag dazu! 💪! \n\n ';
+                    welcome += 'Hast du z. B. die Befürchtung dich infiziert zu haben, kann ich dir helfen zu beurteilen, ob du wirklich zur Risikogruppe gehörst!';
+                    await context.sendActivity();
                 }
             }
 
